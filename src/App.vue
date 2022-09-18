1b1439c5906e25e92787dd62b248a635082d0061
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-09-18 22:47:37 +0200

    feat: realtime

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
-import { computed, watchEffect } from 'vue'
+import { sub } from 'date-fns'
+
+import { computed } from 'vue'
 import { Line } from 'vue-chartjs'
 import 'chartjs-adapter-date-fns'
 
@@ -25,32 +27,57 @@ ChartJS.register(
   LineElement,
   TimeSeriesScale,
   LinearScale,
-  PointElement,
-
+  PointElement
 )
 
-type Record = { time: string, avg: number }
+type Record = { time: string; avg: number }
 type Records = Array<Record>
 
-const { result } = useQuery<{ temperatureRecap: Records }>(gql`
-  query values {
-    temperatureRecap {
+const tick = sub(new Date(), { minutes: 10 }).toISOString()
+const QUERY = gql`
+  query previousValues($tick: timestamptz!) {
+    temperatureRecap(where: { time: { _lte: $tick } }) {
       time
       avg
     }
   }
-`)
+`
 
+const NEW_VALUES = gql`
+  subscription newValues($tick: timestamptz!) {
+    temperatureRecap(where: { time: { _gt: $tick } }) {
+      time
+      avg
+    }
+  }
+`
+
+const { result, subscribeToMore } = useQuery<{ temperatureRecap: Records }>(
+  QUERY,
+  { tick }
+)
+
+subscribeToMore({
+  document: NEW_VALUES,
+  variables: { tick },
+  updateQuery: (previousResult, { subscriptionData }) => {
+    const previous: Records = previousResult?.temperatureRecap || []
+    const next: Records = subscriptionData.data.temperatureRecap || []
+    return { temperatureRecap: [...previous.filter((r) => !next.find((n) => n.time === r.time)), ...next] }
+  }
+})
 
 const chartData = computed<ChartData<'line', Array<any>>>(() => ({
-  datasets: [{
-    label: "Temperature",
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1,
-    fill: false,
+  datasets: [
+    {
+      label: 'Temperature',
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1,
+      fill: false,
 
-    data: result.value?.temperatureRecap || []
-  }],
+      data: result.value?.temperatureRecap || []
+    }
+  ]
 }))
 
 const chartOptions: TChartOptions<'line'> = {
@@ -60,7 +87,6 @@ const chartOptions: TChartOptions<'line'> = {
     yAxisKey: 'avg'
   },
   scales: {
-
     x: {
       type: 'time'
     }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,43 @@ import { DefaultApolloClient } from '@vue/apollo-composable'
 
 import App from './App.vue'
 
+const BACKEND_URL = 'https://ewjsnnbowmzrfyouyuxn.nhost.run/v1/graphql'
+
+import { HttpLink, split } from '@apollo/client/core'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: BACKEND_URL
+})
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: BACKEND_URL.replace(/^http/, 'ws'),
+  options: {
+    reconnect: true
+  }
+})
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
 // Create the apollo client
 const apolloClient = new ApolloClient({
-  uri: 'https://ewjsnnbowmzrfyouyuxn.nhost.run/v1/graphql',
+  link,
   cache: new InMemoryCache()
 })
 
